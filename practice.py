cffe8265a141e5eb6a895a79e0d123a9c140e571
jacob <lalickerj28@cbchs.org>
2024-10-23 10:05:37 -0500

    Indepented project new repository

---
+++ practice.py
@@ -0,0 +1,13 @@
+"""
+Mohamad_Solah = (f"He is {'5ft 9in'},He weighs {'165lbs'},His Shooting Accuracy is {'44%'}, and his Passing Accuracy is {'72.5%'}")
+Question = input('What would you like to learn about?')
+if Question == ('I would like to learn about Mohamad Solah'):
+    print(Mohamad_Solah)
+"""
+
+while True:
+    hand_balls = ("Touching the ball with yor hands is not allowed unless you the goalie and the ball is in the goalies box or you are throwing in the ball after it goes out of ball!")
+    message = ("Greetings User, thanks for using my app!")
+    question = ("What rules of soccer would you like to learn about?")
+    if question == ('I would like to learn about Hand Balls'):
+        print(hand_balls)
